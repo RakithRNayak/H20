5a2006c8e61d15ad9b1902066bcb8752e6720585
Rakith R Nayak <rakithrnayak@gmail.com>
2023-07-19 21:41:30 +0530

    last name changed from public to private

---
+++ src/main/java/com/healthcare/HealthCare/Entity/Patient.java
@@ -0,0 +1,48 @@
+package com.healthcare.HealthCare.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "patients")
+
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    public String lastName;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
